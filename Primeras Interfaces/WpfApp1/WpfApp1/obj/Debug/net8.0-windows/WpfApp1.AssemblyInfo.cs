852c52ea7e41e206c3b08934742d433a99bf180c
Miguel-León <159438587+Sefirr-dot@users.noreply.github.com>
2024-10-10 19:15:02 +0200

    11

--- Primeras Interfaces/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ Primeras Interfaces/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7323785a6240664078e4963fb12571b3cd18b93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d07c7533620affaa124307f0371767e7dcdcd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Primeras Interfaces/WpfApp1/WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Random rd = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Ellipse elip = new Ellipse();
+               
+
+                if (i%2 == 0)
+                {
+                    elip.Fill = Brushes.Coral;
+
+                }
+                else
+                {
+                    elip.Fill= Brushes.Chartreuse;
+                }
+                elip.Width = 10;
+                elip.Height = 10;
+
+               
+
+                Canvas.SetTop(elip, rd.Next(1, (int)this.Height));
+                Canvas.SetLeft(elip, rd.Next(1, (int)this.Width));
+                Canvas.SetZIndex(elip, rd.Next(1,10));
+
+                canva.Children.Add(elip);
+            }
+        }
+    }
+}
